71eaca974899061a917591cb80862830810512ad
grahamhealy <grahamh220@gmail.com>
2018-05-01 21:56:34 +0100

    Adding icon and electron build support.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ let win
   
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 1600, height: 1200, title: "Gmail"})
+    win = new BrowserWindow({width: 1600, height: 1000, title: "Gmail"})
+    win.setMenu(null);
   
     // and load the index.html of the app.
     win.loadURL(url.format({
